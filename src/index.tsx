2f19fe9c64ec76d0c28968e31b6d52a91dde7dc5
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-05-30 19:38:20 +0700

    feat: add fluent ui react components setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 
 const root = createRoot(document.body);
-root.render(<App />);
+root.render(
+    <FluentProvider theme={webLightTheme}>
+        <App />
+    </FluentProvider>
+);
